0de4d346c2c4801fe548c08211ef591a9c9e634e
Anya Brun <tsbrun@icloud.com>
2020-07-27 17:22:11 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,9 @@
-
-
+def my_collect(collection)
+  i = 0
+  modified_collection = []
+  while i < collection.length
+    modified_collection << yield(collection[i])
+    i += 1
+  end
+  modified_collection
+end
